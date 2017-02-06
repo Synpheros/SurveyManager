79aed7ee739e4ef6db4f670836c4916ed80d3494
Iván J. Pérez <synpheros@hotmail.com>
2017-02-06 23:07:48 +0100

    Font

--- routes/clase.js
+++ routes/clase.js
@@ -127,7 +127,7 @@ module.exports = function(auth){
 		classroom.load(function(err, result){
 			var pdf = require('html-pdf');
 
-			var html = '<!DOCTYPE html><html><head><title></title><style type="text/css">body{padding:10px} table{font-size: 26px;font-family: "DejaVu Sans Mono"; border: solid 2px black;border-collapse: collapse;}table td{border: solid 2px black;text-align: center;}</style></head>';
+			var html = '<!DOCTYPE html><html><head><title></title><style type="text/css">body{padding:10px} table{font-size: 18px;font-family: "DejaVu Sans Mono"; border: solid 2px black;border-collapse: collapse;}table td{border: solid 2px black;text-align: center;}</style></head>';
 			html += '<body><table width="100%" style=""><tr><th colspan="6" style="text-align:left">Clase '+classroom.key+':</th></tr><tr><td width="5%">No.</td><td width="45%">Nombre</td><td width="40%" colspan="4">Código</td></tr>';
 
 			for(var i = 0; i < classroom.codes.length; i++){
